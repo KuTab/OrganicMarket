c68e5d9e0abed16017907c1259927280f4957337
Egor Dadugin <egor140202@yandex.ru>
2023-04-18 17:33:33 +0300

    Initial Commit

---
+++ OrganicMarket/OrganicMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganicMarketApp.swift
+//  OrganicMarket
+//
+//  Created by Egor Dadugin on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct OrganicMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
